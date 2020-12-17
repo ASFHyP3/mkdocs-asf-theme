08ab44c0d4e9ebb6f08eccfe822b1d40cfaeb49d
Joseph H Kennedy <me@jhkennedy.org>
2020-12-17 11:36:16 -0900

    fix uninstalled message

--- asf_theme/__init__.py
+++ asf_theme/__init__.py
@@ -7,7 +7,7 @@ try:
 except PackageNotFoundError:
     print('package is not installed!\n'
           'Install in editable/develop mode via (from the top of this repo):\n'
-          '   python -m pip install -e .[develop]\n'
+          '   python -m pip install -e .\n'
           'Or, to just get the version number use:\n'
           '   python setup.py --version')
 
